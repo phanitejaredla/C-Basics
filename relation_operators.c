91a2fe644bc7a6002ca7f096cbabee1840b87bff
phanitejaredla <85404955+phanitejaredla@users.noreply.github.com>
2021-11-08 10:15:17 +0530

    Add files via upload

---
+++ relation_operators.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int a,b;
+	printf("enter values of a and b: ");
+	scanf("%d %d",&a,&b);
+	printf("%d > %d --> %d\n",a,b,a>b);
+	printf("%d >= %d --> %d\n",a,b,a>=b);
+	printf("%d < %d --> %d\n",a,b,a<b);
+	printf("%d <= %d --> %d\n",a,b,a<=b);
+	printf("%d == %d --> %d\n",a,b,a==b);
+	printf("%d != %d --> %d\n",a,b,a!=b);
+}
